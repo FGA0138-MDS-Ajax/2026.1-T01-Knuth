ba7acaca5fbd1af53a1851c1e75b9acc8d944021
Giovana Ferreira <giovana79155@gmail.com>
2026-06-04 12:25:00 -0300

    Fix: bug de tela nao aparecendo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,17 @@
 import './App.css'
 import AuthScreen from './components/auth/AuthScreen'
 import RegisterScreen from './components/auth/RegisterScreen'
+import HomeScreen from './components/home/HomeScreen'
 import { Navigate, Route, Routes } from 'react-router-dom'
-
-function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-950 text-white">
-      <div className="text-center">
-        <h1 className="text-3xl font-bold mb-2">Home</h1>
-        <p className="text-slate-300">A tela de login fica em /login.</p>
-      </div>
-    </div>
-  )
-}
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
+      {/*<Route path="/" element={<Navigate to="/login" replace />} /> APENAS PARA NAO TER QUE FAZER O LOGIN ENQUANTO O HOME EH FEITO*/}
+      <Route path="/" element={<HomeScreen />} />
       <Route path="/login" element={<AuthScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
-      <Route path="/home" element={<Home />} />
+      <Route path="/home" element={<HomeScreen />} />
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   )
