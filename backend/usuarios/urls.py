be5b959a8714da12106bead5fac6ce54369aa88f
Danielly Reis <reisdanny209@gmail.com>
2026-06-06 13:50:39 -0300

    fix: correções de pastas Dispensáveis

--- backend/usuarios/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.fazer_login, name='login'),
-    path('cadastro/', views.cadastrar_usuario, name='cadastro'),
-]
